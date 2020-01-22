26a00a38d9d43bf922b397112e155091f4e73887
Joseph S. Cha <josephshincha@gmail.com>
2020-01-22 19:32:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] = dennis_language 
-     return dennis_language.to_s
+    return programmer_hash[:dennis_ritchie][:languages]
+     
 end
 
 def adding_matz
