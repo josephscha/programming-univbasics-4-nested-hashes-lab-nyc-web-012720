7b3d5bddf2d3e5f85921c544fdf7340bec0cffdd
Joseph S. Cha <josephshincha@gmail.com>
2020-01-22 19:42:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  "Assembly" => programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages] 
   return programmer_hash
 end
